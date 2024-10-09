03816b17e25baa342d39263bced5783251f0a08e
ViktorOnyedim <viktoronyedim@gmail.com>
2024-10-09 01:12:41 +0100

    Modified readme

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+    objects_arr = [
+        {
+            id: 1,
+            firstName: "Guillaume",
+            location: "San Francisco"
+        },
+        {
+            id: 2,
+            firstName: "James",
+            location: "Columbia"
+        },
+        {
+            id: 3,
+            firstName: "Serana",
+            location: "San Francisco"
+        }
+    ];
+
+    return objects_arr;
+};
