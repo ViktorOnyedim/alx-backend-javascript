f26c462c217cb5ec1c6ec2b16720b01fcf8a5b07
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-27 17:18:05 +0100

    Moved on_exit functionality upwards

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,11 @@ process.stdin.setEncoding('utf-8');
 
 console.log("Welcome to Holberton School, what is your name?");
 
+process.on('exit', () => {
+    console.log("This important software is now closing")
+});
 
 process.stdin.on('data', (INPUT) => {
     console.log(`Your name is: ${INPUT.trim()}`);
     process.exit()
 });
-
-process.on('exit', () => {
-    console.log("This important software is now closing")
-})
