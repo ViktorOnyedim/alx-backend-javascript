3e218399cc98d0a770dc6820be536dc15f068eb2
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-28 15:32:22 +0100

    Changed output to use STDOUT

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdin.setEncoding('utf-8');
 
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on('data', (INPUT) => {
+    process.stdout.write(`Your name is: ${INPUT.trim()}\n`);
+    process.exit()
+});
 
 process.on('exit', () => {
     console.log("This important software is now closing")
-});
-
-process.stdin.on('data', (INPUT) => {
-    console.log(`Your name is: ${INPUT.trim()}`);
-    process.exit()
 });
