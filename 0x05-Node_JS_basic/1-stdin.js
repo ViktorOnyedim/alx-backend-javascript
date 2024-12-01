bab279d8134c8bde87f324cba5f8d3b8c9f3034b
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-01 04:07:32 +0100

    Fisxed eslint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,9 +4,9 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('data', (INPUT) => {
     process.stdout.write(`Your name is: ${INPUT.trim()}\n`);
-    process.exit()
+    process.exit();
 });
 
 process.on('exit', () => {
-    console.log("This important software is now closing")
+    console.log("This important software is now closing");
 });
