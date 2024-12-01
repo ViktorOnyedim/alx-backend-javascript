27bf88deda5df9a499f5bbd0b6634fdc8af8eb1f
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-01 04:13:30 +0100

    Fixed eslint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (INPUT) => {
-    process.stdout.write(`Your name is: ${INPUT.trim()}\n`);
-    process.exit();
+  process.stdout.write(`Your name is: ${INPUT.trim()}\n`);
+  process.exit();
 });
 
 process.on('exit', () => {
-    console.log("This important software is now closing");
+  console.log("This important software is now closing");
 });
