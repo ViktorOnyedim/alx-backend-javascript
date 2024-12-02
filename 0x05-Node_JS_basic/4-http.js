c54933306b097b0d9ad90ac9e6d1575107b5a73d
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-02 11:29:40 +0100

    Created small HTTP server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('node:http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Holerton School!');
+});
+
+const PORT = 1245;
+
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}/`);
+})
