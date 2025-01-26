d13a4f73239423f872ed4af03525c4f598a2abed
ViktorOnyedim <viktoronyedim@gmail.com>
2025-01-26 13:58:58 +0100

    Added test suites

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,6 +2,18 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
+    it("rounding of a", () => {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+        assert.strictEqual(calculateNumber(3, 6.1), 9);
+        assert.strictEqual(calculateNumber(0, 1.3), 1);
+    });
+    
+    it("rounding of b", () => {
+        assert.strictEqual(calculateNumber(1.4, 3.4), 4);
+        assert.strictEqual(calculateNumber(3, 6.6), 10);
+        assert.strictEqual(calculateNumber(0, 1.2), 1);
+    });
+
     it("should return 4", () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
