931f3526e7ed52a6911af13eac0ea0cae1ffdad1
ViktorOnyedim <viktoronyedim@gmail.com>
2025-01-26 14:56:02 +0100

    updated api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,36 +1,77 @@
 const express = require('express');
 
 const app = express();
+const PORT = 7865;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id(\\d+)', (req, res) => {
-    res.send(`Payment methods for cart ${req.params.id}`);
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
 });
 
-app.get('/available_payments', (req, res) => {
-    res.json({
-        payment_methods: {
-            credit_cards: true,
-            paypal: false
-        }
-    });
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
 app.post('/login', (req, res) => {
-    let username = '';
-  
-    if (req.body) {
-        username = req.body.userName;
-    }
-  
-    res.send(`Welcome ${username}`);
-  });
+  let username = '';
 
-app.listen(7865, () => {
-    console.log('API available on localhost port 7865');
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
+
+
+
+
+
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the payment system');
+// });
+
+// app.get('/cart/:id(\\d+)', (req, res) => {
+//     res.send(`Payment methods for cart ${req.params.id}`);
+// });
+
+// app.get('/available_payments', (req, res) => {
+//     res.json({
+//         payment_methods: {
+//             credit_cards: true,
+//             paypal: false
+//         }
+//     });
+// });
+
+// app.post('/login', (req, res) => {
+//     let username = '';
+  
+//     if (req.body) {
+//         username = req.body.userName;
+//     }
+  
+//     res.send(`Welcome ${username}`);
+//   });
+
+// app.listen(7865, () => {
+//     console.log('API available on localhost port 7865');
+// });
+
+// module.exports = app;
