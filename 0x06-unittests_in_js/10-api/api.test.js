1bbb883fb4ff5380205c12c634e20e1c2a4bbe68
ViktorOnyedim <viktoronyedim@gmail.com>
2025-01-26 14:45:59 +0100

    updated GET / test

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -4,9 +4,10 @@ const { expect } = require('chai');
 describe('GET /', () => {
     const API_URL = "http://localhost:7865";
 
-    it('should return correct status code 200', (done) => {
-        request.get(API_URL, (error, response, body) => {
+    it('GET / should return correct status code 200', (done) => {
+        request.get(`${API_URL}/`, (error, response, body) => {
             expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome to the payment system');
             done();
         });
     });
