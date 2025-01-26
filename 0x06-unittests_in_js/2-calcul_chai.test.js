e5fa0da256d13259a54dd3aca1e97a2102dce61c
ViktorOnyedim <viktoronyedim@gmail.com>
2025-01-26 15:05:15 +0100

    updated test suites

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,53 +1,54 @@
 const { expect } = require('chai');
-const calculateNumber = require('./2-calcul_chai');
+const calculateNumber = require('./1-calcul_chai');
 
+describe("calculateNumber", () => {
+    describe('SUM', () => {
+        it("It should return 6", () => {
+            expect(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
 
-describe('SUM', () => {
-    it("It should return 6", () => {
-        expect(calculateNumber('SUM', 1.4, 4.5), 6);
+        it("It should return 9", () => {
+            expect(calculateNumber('SUM', 4.3, 6.4), 9);
+        });
+
+        it("should return 7", () => {
+            expect(calculateNumber('SUM', 5, 1.5), 7);
+        });
+
+        it("should return -5", () => {
+            expect(calculateNumber('SUM', -3.2, -2.4), -5);
+        });
     });
 
-    it("It should return 9", () => {
-        expect(calculateNumber('SUM', 4.3, 6.4), 9);
+    describe('SUBTRACT', () => {
+        it("It should return -4", () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+
+        it("It should return -2", () => {
+            expect(calculateNumber('SUBTRACT', 2, 4), -2);
+        });
+
+        it("should return -3", () => {
+            expect(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+        });
+
+        it("should return 2", () => {
+            expect(calculateNumber('SUBTRACT', -2.5, -5), 2);
+        })
     });
 
-    it("should return 6", () => {
-        expect(calculateNumber('SUM', 5, 1.5), 6);
-    });
+    describe('DIVIDE', () => {
+        it("should return 0.2", () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        });
 
-    it("should return -5", () => {
-        expect(calculateNumber('SUM', -3.2, -2.4), -5);
-    });
-});
+        it("should return Error", () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        });
 
-describe('SUBTRACT', () => {
-    it("It should return -4", () => {
-        expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    });
-
-    it("It should return -2", () => {
-        expect(calculateNumber('SUBTRACT', 2, 4), -2);
-    });
-
-    it("should return -3", () => {
-        expect(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
-    });
-
-    it("should return 2", () => {
-        expect(calculateNumber('SUBTRACT', -2.5, -5), 2);
-    })
-});
-
-describe('DIVIDE', () => {
-    it("should return 0.2", () => {
-        expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    });
-
-    it("should return Error", () => {
-        expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    });
-
-    it("should return 2", () => {
-        expect(calculateNumber('DIVIDE', 8, 4), 2);
+        it("should return 2", () => {
+            expect(calculateNumber('DIVIDE', 8, 4), 2);
+        });
     });
 });
